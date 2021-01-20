ff329a660c3adfcc542ca4767e8c13741bd09582
juliano <julianoalp@comp.ufla.br>
2021-01-19 22:48:47 -0300

    teste commit

--- src/main/java/com/controleDeCafe/ControleDeCafeApplication.java
+++ src/main/java/com/controleDeCafe/ControleDeCafeApplication.java
@@ -8,6 +8,7 @@ public class ControleDeCafeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleDeCafeApplication.class, args);
+		System.out.println("Teste de commit");
 	}
 
 }
